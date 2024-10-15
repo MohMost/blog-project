0b74f8522f94fd2a054edd4b5cef181a427d316f
MohMost <teemohmost2020@gmail.com>
2024-10-15 16:35:12 +0100

    🐞 (fix) : api router fix3

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prismadb";
-
-const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
-
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
